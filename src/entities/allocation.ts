d7f177d1a6764ee12a5b98ab6363b5ec45bb5a58
Otavio Lemos <otaviolemos@gmail.com>
2022-09-26 20:26:20 -0300

    feat: should return allocated batch reference

--- src/entities/allocation.ts
+++ src/entities/allocation.ts
@@ -1,12 +1,16 @@
 import { Batch } from './batch'
 import { OrderLine } from './order-line'
 
-export function allocate (line: OrderLine, batches: Batch[]): void {
+export function allocate (line: OrderLine, batches: Batch[]): string {
   const allocatableBatches = batches.filter(batch => batch.canAllocate(line))
   let selectedBatch = allocatableBatches[0]
   for (const batch of allocatableBatches) {
-    if (batch.eta === undefined) return batch.allocate(line)
+    if (batch.eta === undefined) {
+      batch.allocate(line)
+      return batch.reference
+    }
     if (batch.eta < selectedBatch.eta) selectedBatch = batch
   }
   selectedBatch.allocate(line)
+  return selectedBatch.reference
 }
