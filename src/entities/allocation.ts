804761065867240c1f1ffdb79c1bdd2add3048e2
Otavio Lemos <otaviolemos@gmail.com>
2022-09-26 20:12:24 -0300

    feat: should prefer earlier batches

--- src/entities/allocation.ts
+++ src/entities/allocation.ts
@@ -3,6 +3,13 @@ import { OrderLine } from './order-line'
 
 export function allocate (line: OrderLine, batches: Batch[]): void {
   const allocatableBatches = batches.filter(batch => batch.canAllocate(line))
-  const adequateBatch = allocatableBatches.find(batch => batch.eta === undefined)
+  const batchesSortedByEta = allocatableBatches.sort(compareByEta)
+  const adequateBatch = batchesSortedByEta[0]
   adequateBatch.allocate(line)
+}
+
+function compareByEta (b1: Batch, b2: Batch): number {
+  if (b1.eta === undefined || b1.eta < b2.eta) return -1
+  if (b2.eta === undefined || b1.eta > b2.eta) return 1
+  return 0
 }
