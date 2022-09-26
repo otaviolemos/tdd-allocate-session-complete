99056e248759d20f43278e3fa40f3d63ab8d2bd3
Otavio Lemos <otaviolemos@gmail.com>
2022-09-26 20:31:40 -0300

    feat: should handle out of stock errors

--- src/entities/allocation.ts
+++ src/entities/allocation.ts
@@ -1,8 +1,11 @@
 import { Batch } from './batch'
 import { OrderLine } from './order-line'
+import { OutOfStockError } from './out-of-stock-error'
 
 export function allocate (line: OrderLine, batches: Batch[]): string {
   const allocatableBatches = batches.filter(batch => batch.canAllocate(line))
+  if (allocatableBatches.length === 0) throw new OutOfStockError()
+
   let selectedBatch = allocatableBatches[0]
   for (const batch of allocatableBatches) {
     if (batch.eta === undefined) {
