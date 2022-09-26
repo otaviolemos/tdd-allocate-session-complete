f0e7e17da1b88234254543f08f240609f786efe1
Otavio Lemos <otaviolemos@gmail.com>
2022-09-26 20:22:44 -0300

    refactor: improve performance

--- src/entities/allocation.ts
+++ src/entities/allocation.ts
@@ -3,13 +3,10 @@ import { OrderLine } from './order-line'
 
 export function allocate (line: OrderLine, batches: Batch[]): void {
   const allocatableBatches = batches.filter(batch => batch.canAllocate(line))
-  const batchesSortedByEta = allocatableBatches.sort(compareByEta)
-  const adequateBatch = batchesSortedByEta[0]
-  adequateBatch.allocate(line)
-}
-
-function compareByEta (b1: Batch, b2: Batch): number {
-  if (b1.eta === undefined || b1.eta < b2.eta) return -1
-  if (b2.eta === undefined || b1.eta > b2.eta) return 1
-  return 0
+  let selectedBatch = allocatableBatches[0]
+  for (const batch of allocatableBatches) {
+    if (batch.eta === undefined) return batch.allocate(line)
+    if (batch.eta < selectedBatch.eta) selectedBatch = batch
+  }
+  selectedBatch.allocate(line)
 }
